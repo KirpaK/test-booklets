c16a4fb08063b5ef103df29951bc67076a10c7ac
Kirill Kirpa <kirillkirpa@gmail.com>
2019-04-03 13:51:59 +0500

    Дополнил архитектуру

--- test-booklets/src/store/index.js
+++ test-booklets/src/store/index.js
@@ -1,10 +1,11 @@
 import { createBrowserHistory } from "history";
 import configureStore from "./configureStore";
+import animals from "./modules/animals";
 
 export const history = createBrowserHistory({ basename: "/" });
 
 const modules = {
-  
+  animals
 };
 
 const initialState = window.initialReduxState;
